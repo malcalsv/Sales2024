ef2176d4a72ddc289b431ea372361eefa08c1872
malcalsv <55215815+malcalsv@users.noreply.github.com>
2024-03-18 14:54:19 -0600

    Project Archictecture

---
+++ Sales/Sale.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Sale.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Sales/Sale.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Sale.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
