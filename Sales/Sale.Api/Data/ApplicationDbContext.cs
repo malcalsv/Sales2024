4251a29f087a8ac8dbdcf2b46196e54e3676f8d3
malcalsv <55215815+malcalsv@users.noreply.github.com>
2024-03-18 16:27:14 -0600

    Creacion del primer modelo, migracion y actulizacion de la BD para contries

---
+++ Sales/Sale.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.Shared.Entities;
+
+namespace Sale.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options) 
+        {
+            
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        //Crear indice unido en la entidad countries
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+        }
+
+    }
+}
---
+++ Sales/Sales.Shared/Entities/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Pais")]
+        [Required(ErrorMessage = "el campo {0} es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El campo {0} solo puede ser mayor de {1} caracteres")]
+        public string Name { get; set; } = null!;
+
+    }
+}
